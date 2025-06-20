fd172caa9066f1d7ecffa842f140f4e1f09791fe
cassjbeckford <cassjbeckford282@outlook.com>
2025-06-20 16:45:57 +0100

    add home and not found pages and import to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {Home} from "./pages/Home"
+import {NotFound} from "./pages/NotFound"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={} />
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
